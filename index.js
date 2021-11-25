858f17190753cd2444ffe8eb87cce9928e1b43f6
olga210600 <89257451+olga210600@users.noreply.github.com>
2021-11-25 11:34:42 +0200

    added big photo

--- index.js
+++ index.js
@@ -1,110 +1,75 @@
-const photosList = document.querySelector('.photos');
-const getPostsBtn = document.querySelector('.addBtn');
-let img = document.querySelectorAll('.clickable');
+    const photosList = document.querySelector('.photos');
+    const getPostsBtn = document.querySelector('.addBtn');
+    let img = document.querySelectorAll('.clickable');
+    const wrapper = document.querySelector('.mainWrapper');
 
-// console.log(img)
-// const imgArray = []
-
-const state = {
-    photos: [],
-}
-
-const createPhotos = (photoItem) => {
-    console.log('photos: ', photoItem)
-    return `
-    <div class = "photoWrapper">
-        <div class="post">
-<!--            <div class="wrapperBody">${photoItem.body}</div>-->
-            <img class="clickable" src="${photoItem.url}"> 
-        </div>
-    </div>
-       
-`
-
-//
-
-}
-// let arrayPhotos = photos
-// console.log(arrayPhotos)
-
-// window.addEventListener('load', function() {
-//     let images = document.querySelectorAll('.clickable');
-//     /* images - это NodeList, объект, похожий на обычный массив, но не совсем. Нам необходимо перебрать все элементы images и повесить им обработчик клика */
-//     images.forEach(function(image) {
-//         image.addEventListener('click', imageClickFunc);
-//     });
-//
-//     /* Теперь осталось написать сам обработчик. Это обычная функция, для которой this будет кликнутый элемент */
-//
-//     function imageClickFunc() {
-//         alert('Привет! Ты кликнул на ' + this);  // ну или любой другой код
-//     }
-// });
-
-
-const fillPostList = (photos) => {
-    photosList.innerHTML = "";
-
-    if (photos.length) {
-        photos.forEach((photo, index) => photosList.innerHTML += createPhotos(photo, index))
+    const state = {
+        photos: [],
     }
-    aaa = photos
+
+    function getPhotos() {
+        return fetch('https://jsonplaceholder.typicode.com/photos?_limit=15', {
+            headers: {
+                "Content-type": "application/json: charset=YTF-8"
+            }
+
+        })
+            .then((res) => res.json())
+            .then((photos) => state.photos = photos)
+    }
 
 
+    function closeShowedImage() {
+        console.log('remove ')
+        wrapper.removeChild(document.querySelector('.showedImageWrapper'));
+        wrapper.removeChild(document.querySelector('.blackCover'));
+    }
 
 
+    function showImage(index) {
+        const clickedPhoto = state.photos[index];
+        console.log('clickedPhoto: ', clickedPhoto)
 
-// зашла в массив обьектов и получила id
+        const blackCover = document.createElement('div');
+        blackCover.className = 'blackCover'
+        blackCover.onclick = closeShowedImage;
 
-    photos.forEach(elements => {
-        if (elements.id === 124444){
-            console.log('ttttttthere', true)
+        const showedImageWrapper = document.createElement('div');
+        showedImageWrapper.className = 'showedImageWrapper'
+
+        const closeIcon = document.createElement('span')
+        closeIcon.innerHTML = "&#10006";
+        closeIcon.className = 'closeIcon';
+        closeIcon.onclick = closeShowedImage;
+
+        const showedImage = document.createElement('img')
+        showedImage.className = 'showedImage';
+        showedImage.src = clickedPhoto.url;
+
+        showedImageWrapper.appendChild(closeIcon);
+        showedImageWrapper.appendChild(showedImage);
+        wrapper.appendChild(showedImageWrapper);
+        wrapper.appendChild(blackCover);
+    }
+
+    const createPhotos = (photoItem, index) => `
+            <div onclick="showImage(${index})" class = "photoWrapper">
+                <div class="post">
+                    <img class="clickable" src="${photoItem.url}"> 
+                </div>
+            </div>
+        `
+
+    const fillPostList = (photos) => {
+        photosList.innerHTML = "";
+
+        if (photos.length) {
+            photos.forEach((photo, index) => photosList.innerHTML += createPhotos(photo, index))
         }
-        console.log(false)
+    }
+
+    getPostsBtn.addEventListener('click', async () => {
+        await getPhotos();
+        fillPostList(state.photos);
     })
 
-
-}
-
-// aaa.addEventListener('mouseover', () => {
-//     console.log('hey im therebbbbbbbbbbbbbbbbbbb')
-// })
-
-
-getPostsBtn.addEventListener('click', async () => {
-    await getPhotos();
-    fillPostList(state.photos);
-    // console.log('juntyhntntyhnyt' ,state)
-    //
-    // imgArray.push(img)
-    //
-    // console.log('imggggg', imgArray)
-
-})
-
-// img.addEventListener('click', () =>{
-//     console.log( Object.entries(state))
-//
-// })
-
-
-
-function getPhotos() {
-    return fetch('https://jsonplaceholder.typicode.com/photos?_limit=15', {
-        headers: {
-            "Content-type": "application/json: charset=YTF-8"
-        }
-
-    })
-        .then((res) => res.json())
-        .then((photos) => state.photos = photos)
-
-}
-
-// state.photos.push(getPhotos())
-//
-// console.log('photoss',state)
-//
-// // state.photos.forEach(element => console.log(  'elements', element[1]))
-
-// console.log(arrImg)
